17a3e9d925da8da65621f32c32d4894f5f9c499f
sandeep-kategar <sandeep.skk12@gmail.com>
2026-02-15 11:37:51 +0530

    Relative path is added
    Relative path is added

--- model/model_comparison.py
+++ model/model_comparison.py
@@ -1,3 +1,8 @@
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+
 import pandas as pd
 
 from model.logistic_regression import train_logistic_regression
